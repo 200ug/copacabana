018ff819563f61a5f19c2ac76871871b235d6ea8
200ug <a@dyyni.org>
2026-01-04 19:14:31 +0200

    fix: match header file signature with implementation

--- src/uint256.h
+++ src/uint256.h
@@ -14,7 +14,7 @@ int kad_uint256_cmp(const kad_uint256_t *a, const kad_uint256_t *b);
 int kad_uint256_clz(const kad_uint256_t *a); // bucket index calculations
 int kad_uint256_is_zero(const kad_uint256_t *a);
 void kad_uint256_xor(kad_uint256_t *r, const kad_uint256_t *a, const kad_uint256_t *b);
-void kad_uint256_from_bytes(kad_uint256_t *r, const uint8_t bytes[32]);
+void kad_uint256_from_bytes(kad_uint256_t *r, const uint8_t bytes[20]);
 void kad_uint256_from_key(kad_uint256_t *r, const uint8_t *key, const size_t len);
 void kad_uint256_random(kad_uint256_t *r);
 
